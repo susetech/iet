e68c25519e6ebcf130ce679f8bb446df208bd446
susetech <susetechnologies@gmail.com>
2019-06-23 16:57:45 +0530

    Initial Commit

---
+++ innovative-education-tool/src/main/java/com/suse/eet/innovative_education_tool/App.java
@@ -0,0 +1,13 @@
+package com.suse.eet.innovative_education_tool;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
